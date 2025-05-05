12913c17975427c44183c1cf302e712403697a91
Thomas Butler <trbiv@proton.me>
2025-05-04 21:53:46 -0500

    updating to use rust

--- get_example_data.sh
+++ get_example_data.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000_genomes_project/release/20190312_biallelic_SNV_and_INDEL/ALL.chr22.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz
 mkdir data
-gunzip -c ALL.chr22.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz > data/genome_example.vcf
+wget -P data ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000_genomes_project/release/20190312_biallelic_SNV_and_INDEL/ALL.chr22.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use flate2::read::MultiGzDecoder;
+use std::fs::File;
+use std::io::BufReader;
+use vcf::{U8Vec, VCFError, VCFHeaderFilterAlt, VCFReader, VCFRecord};
+
+fn main() -> Result<(), VCFError> {
+    let mut reader = VCFReader::new(BufReader::new(MultiGzDecoder::new(File::open(
+        "./data/ALL.chr22.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz",
+    )?)))?;
+
+    // access FILTER contents
+    assert_eq!(
+        Some(VCFHeaderFilterAlt {
+            id: b"PASS",
+            description: b"All filters passed"
+        }),
+        reader.header().filter(b"PASS")
+    );
+
+    // prepare VCFRecord object
+    let mut vcf_record = reader.empty_record();
+
+    // read one record
+    reader.next_record(&mut vcf_record)?;
+    println!("{:?}", vcf_record);
+
+    Ok(())
+}
